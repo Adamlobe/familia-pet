c691a442e76a1cf5bc6f95e4b3b5add0514ed73d
Adam Lobe <adamacacio0@gmail.com>
2024-05-13 22:57:48 -0300

    Commitando todo projeto novamente em um novo diretório do GitHub

---
+++ app.py
@@ -0,0 +1,51 @@
+import streamlit as st 
+import pandas as pd
+import os
+
+st.title('**Espaço** Familia & Pets')
+st.subheader('Nunca foi tão fácil achar o lugar perfeito', divider='gray')
+
+df = pd.read_excel('lugares.xlsx')
+
+colunas = df.columns[0:6]
+geoLocation = df[['LATITUDE','LONGITUDE']]
+
+places = [' Selecione um lugar...'] + df['Nome'].unique().tolist()
+places.sort()
+
+filtro = st.sidebar.selectbox("Selecione um lugar:", places)
+
+if filtro == ' Selecione um lugar...':
+    #st.write(df[colunas])
+    st.map(geoLocation)
+else:
+    df_filtrado = df[df['Nome'] == filtro][colunas]
+    
+
+    index_linha_filtrada = df_filtrado.index.tolist()
+
+    #st.write(df_filtrado)
+
+    lugar = df.iloc[index_linha_filtrada[0],1]
+    endereco = df.iloc[index_linha_filtrada[0],2]
+    crianca = df.iloc[index_linha_filtrada[0],3]
+    pet = df.iloc[index_linha_filtrada[0],4]
+    banheiro = df.iloc[index_linha_filtrada[0],5]
+
+    st.subheader('')
+    st.subheader('**Informações**',divider='gray')
+    st.write('Tipo de lugar: ', lugar)
+    st.write('Endereço: ', endereco)
+    st.write('Ideal para criança? ',crianca)
+    st.write('Aceita Pet?', pet)
+    st.write('Possui banheiro família: ', banheiro)
+
+
+    geoLocation_temp = pd.DataFrame({
+        'LATITUDE': [df.iloc[index_linha_filtrada[0], 6]],
+        'LONGITUDE': [df.iloc[index_linha_filtrada[0], 7]]})
+    st.map(geoLocation_temp, zoom=16, size=25)
+    
+    image_path = df.iloc[index_linha_filtrada[0],8]
+    
+    st.image(image_path)
